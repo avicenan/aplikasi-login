ce31322c6368b9a7ee012c713aa0f7b3bd8a094e
avicenan <avicena.n@gmail.com>
2023-09-21 13:15:31 +0700

    memperbaiki format komentar

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,26 +1,26 @@
-// Membuat variabel untuk setiap elemen view.
+/*Comment : Membuat variabel untuk setiap elemen view. */
 const loginFormElement = document.querySelector("#loginForm");
 const inputEmailElement = document.querySelector("#inputEmail");
 const inputPasswordElement = document.querySelector("#inputPassword");
 
-// Mmebuat variabel untuk menyimpan informasi email dan password.
+/*Comment : Membuat variabel untuk menyimpan informasi email dan password. */
 const expectedEmail = "admin@dicoding.com";
 const expectedPassword = "superpass";
 
-// Menambahkan aksi klik pada button.
+/*Comment : Menambahkan aksi klik pada button.*/
 loginFormElement.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  // Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan
+  /*Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
 
-  // Memastika bahwa nilai email dan password sesuai dengan nilai yang tersimpan
+  /*Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan */
   if (email == expectedEmail && password == expectedPassword) {
-    // Jika sesuai maka program akan berpindah ke halaman home.
+    /*Comment : Jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
   } else {
-    // Namun, jika tidak sesuai maka akan menampilkan informasi bahwa input salah
+    /*Comment : Namun, jika tidak sesuai maka akan menampilkan informasi bahwa input salah */
     showPopUp();
   }
 });
